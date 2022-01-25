6901f5ba9bf2b386911d983fe399d5dba7f7ff0c
Fernando Silva <fernando.loures@mobitype.com.br>
2022-01-25 10:04:18 -0300

    test: ensure signup returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from './signup'
+describe('SignUp Controller', () => {
+  test('Should return 400 if name is not povided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
