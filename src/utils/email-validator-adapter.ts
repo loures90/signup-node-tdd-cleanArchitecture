56ec714a7bd1a9f8beb373e3b8cbc1494330503a
Fernando Silva <fernando.loures@mobitype.com.br>
2022-01-31 10:38:01 -0300

    feat: ensure emailValidatorAdapter call correct email value

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -3,7 +3,6 @@ import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    const result = validator.isEmail(email)
-    return result
+    return validator.isEmail(email)
   }
 }
