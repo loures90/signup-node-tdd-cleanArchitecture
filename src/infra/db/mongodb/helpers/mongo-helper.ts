2dc9d1bc6f6f831fb9970dfacf7129ca3d6fdd34
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-08 21:48:02 -0300

    feat: createeate helper methods MongoHelper to connect to Mongo

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: MongoClient,
+  uri: '' as string,
+
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client?.close()
+    this.client = null
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    return this.client.db().collection(name)
+  }
+}
