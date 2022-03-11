37dfc75d3d218711baf1251ebdc672af7cff98d5
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-11 09:08:30 -0300

    refactor: ensure AccountMonfoRepository add method does not return _id value

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,10 +5,10 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (addAccountData: AddAccountModel): Promise<AccountModel> {
+    const accountData = { ...addAccountData }
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(addAccountData)
+    const result = await accountCollection.insertOne(accountData)
     const id = result.insertedId.toString()
-    const account = { ...addAccountData, id: id }
-    return account
+    return { ...addAccountData, id: id }
   }
 }
