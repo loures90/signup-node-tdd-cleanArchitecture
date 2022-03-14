3e568a5eb77e9bc20912fc3f318ef96c9661a8b5
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-14 14:06:06 -0300

    test: ensure Mongo reconnect if connection downs

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,17 @@
+import { MongoHelper as sut } from './mongo-helper'
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+  test('Should reconnect if connection downs', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
