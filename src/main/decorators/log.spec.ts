5e5304594341b38268500bd905ee0539415e3875
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-14 14:21:01 -0300

    refactor: move sut to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,31 +2,52 @@ import { Controller, HttpRequest, HttpResponse } from '../../presentation/protoc
 import { LogControllerDecorator } from './log'
 
 describe('LogController decorator', () => {
-  test('Shoul call call controller handle', async () => {
+  const makeFakeHttpResponse = (): HttpResponse => ({
+    statusCode: 200,
+    body: {
+      email: 'valid_email@email.com',
+      name: 'valid_name',
+      password: 'password'
+    }
+  })
+
+  const makeFakeHttpRequest = (): HttpRequest => ({
+    body: {
+      email: 'valid_email@email.com',
+      name: 'valid_name',
+      password: 'password',
+      passwodConfirmation: 'password'
+    }
+  })
+
+  interface SutTypes {
+    sut: LogControllerDecorator
+    controllerStub: Controller
+  }
+
+  const makeSut = (): SutTypes => {
+    const controllerStub = makeControllerStub()
+    const sut = new LogControllerDecorator(controllerStub)
+    return {
+      sut,
+      controllerStub
+    }
+  }
+
+  const makeControllerStub = (): Controller => {
     class ControllerStub implements Controller {
       async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            email: 'valid_email@email.com',
-            name: 'valid_name',
-            password: 'password'
-          }
-        }
+        const httpResponse: HttpResponse = makeFakeHttpResponse()
         return await new Promise(resolve => resolve(httpResponse))
       }
     }
-    const controllerStub = new ControllerStub()
+    return new ControllerStub()
+  }
+
+  test('Shoul call call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
-    const httpRequest = {
-      body: {
-        email: 'valid_email@email.com',
-        name: 'valid_name',
-        password: 'password',
-        passwordConfirmation: 'password'
-      }
-    }
+    const httpRequest = makeFakeHttpRequest()
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
