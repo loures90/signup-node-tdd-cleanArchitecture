1f39629f389b3e681a25ef6593cb445901d303d0
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-18 09:55:11 -0300

    test: ensure CompareFields Validation return InvalidParamError if validation fails

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,31 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+describe('Compare Fields Validation', () => {
+  interface SutTypes {
+    sut: CompareFieldsValidation
+  }
+  const makeSut = (): SutTypes => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    return {
+      sut
+    }
+  }
+  test('Should return Missing Params if validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'wrong_value'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should return null if validation succed', () => {
+    const { sut } = makeSut()
+    const result = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(result).toEqual(null)
+  })
+})
