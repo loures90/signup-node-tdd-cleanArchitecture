9ebb7247c1967b062330ad66f04172d7d3323c50
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-18 09:45:23 -0300

    test: ensure RequiredFieldValidation return null if validation succed

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,9 +2,24 @@ import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
 describe('Requerid Fields', () => {
-  test('Should return Missing Params if validation fails', () => {
+  interface SutTypes {
+    sut: RequiredFieldValidation
+  }
+  const makeSut = (): SutTypes => {
     const sut = new RequiredFieldValidation('field')
-    const result = sut.validate({})
-    expect(result).toEqual(new MissingParamError('field'))
+    return {
+      sut
+    }
+  }
+  test('Should return Missing Params if validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should return null if validation succed', () => {
+    const { sut } = makeSut()
+    const result = sut.validate({ field: 'any_value' })
+    expect(result).toEqual(null)
   })
 })
