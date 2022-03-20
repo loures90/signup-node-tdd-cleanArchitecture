f417000b636d9a628653c6c2ac60e74df47a8815
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-20 17:33:04 -0300

    refactor: group validations protocols

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  preset: "@shelf/jest-mongodb",
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
