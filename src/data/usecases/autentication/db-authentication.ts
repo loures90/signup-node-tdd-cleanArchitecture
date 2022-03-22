e81b14c6709aa82306e7bf2ef532e6ab0bc2e333
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-22 09:57:37 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository  with correct id and token

--- src/data/usecases/autentication/db-authentication.ts
+++ src/data/usecases/autentication/db-authentication.ts
@@ -1,19 +1,22 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer, LoadAccountByEmailRepository, TokenGenerator } from './db-authentication-protocols'
+import { HashComparer, LoadAccountByEmailRepository, TokenGenerator, UpdateAccessTokenRepository } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator
+    tokenGenerator: TokenGenerator,
+    updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -22,10 +25,14 @@ export class DbAuthentication implements Authentication {
     if (!account) {
       return null
     }
+
     const isValid = await this.hashComparer.compare(password, account.password)
     if (!isValid) {
       return null
     }
-    return await this.tokenGenerator.generate(account.id)
+
+    const accessToken = await this.tokenGenerator.generate(account.id)
+    await this.updateAccessTokenRepository.update(account.id, accessToken)
+    return accessToken
   }
 }
