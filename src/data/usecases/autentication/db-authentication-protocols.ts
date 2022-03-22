8e1770e32e919361010cef63850154c249cde3b1
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-22 09:21:52 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/data/usecases/autentication/db-authentication-protocols.ts
+++ src/data/usecases/autentication/db-authentication-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../protocols/criptography/hash-comparer'
 export * from '../../protocols/db/load-account-by-email-repository'
+export * from '../../protocols/criptography/token-generator'
--- src/data/usecases/autentication/db-authentication.ts
+++ src/data/usecases/autentication/db-authentication.ts
@@ -1,13 +1,19 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer, LoadAccountByEmailRepository } from './db-authentication-protocols'
+import { HashComparer, LoadAccountByEmailRepository, TokenGenerator } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -17,6 +23,7 @@ export class DbAuthentication implements Authentication {
       return null
     }
     await this.hashComparer.compare(password, account.password)
+    await this.tokenGenerator.generate(account.id)
     return null
   }
 }
