558daf83c1ef56a604e9391a9b5406acded681c8
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-22 08:44:34 -0300

    test: ensure DbAuthentication return nulls if HashCompare returns false

--- src/data/usecases/autentication/db-authentication.spec.ts
+++ src/data/usecases/autentication/db-authentication.spec.ts
@@ -85,4 +85,11 @@ describe('Signup DbAddAccount ', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve) => resolve(false)))
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe(null)
+  })
 })
