e67a74e1b04c601ed425ad01e954b4e10b515acc
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-24 11:56:33 -0300

    feat: ensure AccountRepository UpdateAccessToken update the accessToken field

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,12 @@
+import { ObjectId } from 'mongodb'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/update-access-token-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add (addAccountData: AddAccountModel): Promise<AccountModel> {
     const accountData = { ...addAccountData }
     const accountCollection = await MongoHelper.getCollection('accounts')
@@ -19,5 +21,15 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     const account = result?._id ? { ...result, id: result?._id } : null
     delete account?._id
     return account
+  }
+
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: new ObjectId(id) },
+      {
+        $set: {
+          accessToken: token
+        }
+      })
   }
 }
