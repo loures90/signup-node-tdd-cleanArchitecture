4409b5b8131f1a71b55c45f982de677f29a2a6d8
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-24 10:38:11 -0300

    refactor: rename update method to updateAccessToken

--- src/data/usecases/autentication/db-authentication.spec.ts
+++ src/data/usecases/autentication/db-authentication.spec.ts
@@ -77,7 +77,7 @@ const makeEncrypter = (): Encrypter => {
 
 const makeUpdateAccessTokenRepository = (): UpdateAccessTokenRepository => {
   class UpdateAccessTokenRepositoryStub implements UpdateAccessTokenRepository {
-    async update (id: string, token: string): Promise<void> {
+    async updateAccessToken (id: string, token: string): Promise<void> {
     }
   }
   return new UpdateAccessTokenRepositoryStub()
@@ -148,14 +148,14 @@ describe('Signup DbAddAccount ', () => {
 
   test('Should calls updateAccessTokenRepository with correct token', async () => {
     const { sut, updateAccessTokenRepositoryStub } = makeSut()
-    const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, 'update')
+    const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, 'updateAccessToken')
     await sut.auth(makeFakeAuthentication())
     expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_access_token')
   })
 
   test('Should throw if updateAccessTokenRepository throws', async () => {
     const { sut, updateAccessTokenRepositoryStub } = makeSut()
-    jest.spyOn(updateAccessTokenRepositoryStub, 'update').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(updateAccessTokenRepositoryStub, 'updateAccessToken').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
