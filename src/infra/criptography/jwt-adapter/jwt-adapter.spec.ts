746591596708840e9606ff012fd083d887c881d1
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-24 10:29:22 -0300

    refactor: move sut to a factory

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -6,9 +6,13 @@ jest.mock('jsonwebtoken', () => ({
     return await new Promise(resolve => resolve('access_token'))
   }
 }))
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
 describe('JWT Adapter', () => {
   test('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({
@@ -17,13 +21,13 @@ describe('JWT Adapter', () => {
   })
 
   test('Should return a token on sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('access_token')
   })
 
   test('Should throws if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error()
     })
