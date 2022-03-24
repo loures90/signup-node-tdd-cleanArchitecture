80fbcec0fda66d5cd7930a511cc2c3e10a740551
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-24 11:05:45 -0300

    feat: ensure AccountRepository loadByEmail returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,14 +1,23 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (addAccountData: AddAccountModel): Promise<AccountModel> {
     const accountData = { ...addAccountData }
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const id = result.insertedId.toString()
     return { ...addAccountData, id: id }
+  }
+
+  async loadByEmail (email: string): Promise<any> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.findOne({ email })
+    const account = { ...result, id: result._id }
+    delete account._id
+    return account
   }
 }
