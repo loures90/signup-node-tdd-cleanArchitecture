9869d77d8219ae2d2887f365326a9f00cd37920c
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-25 09:45:14 -0300

    feat: ensure route api/login return 200 on success

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/clean-node',
-  port: process.env.PORT ?? '5050'
+  port: process.env.PORT ?? '5050',
+  jwt_secret: process.env.JWT_SECRET ?? 'gyto05345WWWW/*-.'
 }
