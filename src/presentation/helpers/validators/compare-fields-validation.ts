a4df0682e3006ada7c4e97f176a0fa36df4963a2
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-25 08:19:33 -0300

    refactor: change class constructors

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -2,13 +2,10 @@ import { InvalidParamError } from '../../errors'
 import { Validation } from '../../protocols/validation'
 
 export class CompareFieldsValidation implements Validation {
-  private readonly fieldName: string
-  private readonly fieldCompareName: string
-
-  constructor (fieldName: string, fieldCompareName: string) {
-    this.fieldName = fieldName
-    this.fieldCompareName = fieldCompareName
-  }
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldCompareName: string
+  ) {}
 
   validate (input: any): Error | null {
     if (input[this.fieldName] !== input[this.fieldCompareName]) {
