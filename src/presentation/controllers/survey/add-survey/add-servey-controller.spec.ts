023bf13c29438b344e24d0bb691359214733e884
Fernando Silva <fernando.loures@mobitype.com.br>
2022-03-30 00:29:25 -0300

    test: ensure AddServeyController calls AddSurvey with correct values

--- src/presentation/controllers/survey/add-survey/add-servey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-servey-controller.spec.ts
@@ -1,7 +1,9 @@
-import { HttpRequest, Validation } from '../../../protocols'
+import { InvalidParamError } from '../../../errors'
+import { badRequest } from '../../../helpers/http/http-helper'
 import { AddSurveyController } from './add-survey-controller'
+import { AddSurvey, AddSurveyModel, HttpRequest, Validation } from './add-survey-protocols'
 
-const makeValidationStub = (): Validation => {
+const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
       return null
@@ -9,17 +11,28 @@ const makeValidationStub = (): Validation => {
   }
   return new ValidationStub()
 }
+
+const makeAddSurvey = (): AddSurvey => {
+  class AddSurveyStub implements AddSurvey {
+    async add (data: AddSurveyModel): Promise<void> {}
+  }
+  return new AddSurveyStub()
+}
+
 interface SutTypes {
   sut: AddSurveyController
   validationStub: Validation
+  addSurveyStub: AddSurvey
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidationStub()
-  const sut = new AddSurveyController(validationStub)
+  const validationStub = makeValidation()
+  const addSurveyStub = makeAddSurvey()
+  const sut = new AddSurveyController(validationStub, addSurveyStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    addSurveyStub
   }
 }
 
@@ -37,6 +50,27 @@ describe('Add Survey', () => {
   test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const surveySpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(surveySpy).toBeCalledWith({
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answers: 'any_answer'
+      }]
+    })
+  })
+
+  test('Should return 400 if validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new InvalidParamError('any_param'))
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('any_param')))
+  })
+
+  test('Should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut()
+    const surveySpy = jest.spyOn(addSurveyStub, 'add')
     await sut.handle(makeFakeRequest())
     expect(surveySpy).toBeCalledWith({
       question: 'any_question',
