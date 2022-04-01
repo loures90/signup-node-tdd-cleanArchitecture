8882f02397017b72fea59a0bc65105d9c4b29efa
Fernando Silva <fernando.loures@mobitype.com.br>
2022-04-01 10:43:28 -0300

    refactor: move Middlewares dependencies to a protocol file

--- src/presentation/middleware/auth-middleware.spec.ts
+++ src/presentation/middleware/auth-middleware.spec.ts
@@ -1,9 +1,7 @@
-import { HttpRequest, HttpResponse } from '../protocols'
+import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken, HttpRequest, HttpResponse, AccountModel } from './auth-middleware-protocols'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
-import { AuthMiddleware } from './auth-middleware'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
-import { AccountModel } from '../../domain/models/account'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
