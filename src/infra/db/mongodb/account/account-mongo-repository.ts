a1dc8e60897de7d861629fbd3efd2c651fa9aea6
Fernando Silva <fernando.loures@mobitype.com.br>
2022-04-04 14:28:12 -0300

    feat: ensure AccountMongoRepository returns an account on loadByToken success without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,12 +1,17 @@
 import { ObjectId } from 'mongodb'
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
+import { LoadAccountByTokenRepository } from '../../../../data/protocols/db/account/load-account-by-token-repository'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+export class AccountMongoRepository implements
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository,
+  LoadAccountByTokenRepository {
   async add (addAccountData: AddAccountModel): Promise<AccountModel> {
     const accountData = { ...addAccountData }
     const accountCollection = await MongoHelper.getCollection('accounts')
@@ -38,5 +43,21 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
           accessToken: token
         }
       })
+  }
+
+  async loadByToken (token: string, role?: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.findOne({ accessToken: token })
+    console.log(result)
+    if (!result?._id) {
+      return null
+    }
+    const account = {
+      id: result._id.toString(),
+      name: result.name,
+      email: result.email,
+      password: result.password
+    }
+    return account
   }
 }
