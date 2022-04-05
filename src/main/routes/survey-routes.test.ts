0707c35b6a65723057dddcaa729e62abc0230bf1
Fernando Silva <fernando.loures@mobitype.com.br>
2022-04-05 09:42:00 -0300

    test: ensure add-survey return 204 on valid access-token

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,6 +1,11 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+import { ObjectId } from 'mongodb'
+
+let accountsCollection
 
 describe('Surveys Routes', () => {
   beforeAll(async () => {
@@ -14,6 +19,8 @@ describe('Surveys Routes', () => {
   beforeEach(async () => {
     const surveysCollection = await MongoHelper.getCollection('surveys')
     await surveysCollection.deleteMany({})
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
   })
 
   describe('POST Add-Survey', () => {
@@ -28,6 +35,35 @@ describe('Surveys Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should 204 on add-survey success with valid-token', async () => {
+      const result = await accountsCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const id = result.insertedId.toString()
+      const accessToken = sign({ id }, env.jwt_secret)
+      await accountsCollection.updateOne({ _id: new ObjectId(id) },
+        {
+          $set: {
+            accessToken
+          }
+        })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }]
+        })
+        .expect(204)
     })
   })
 })
