d30433591405bbca1bd7d7c546137745ca1f321c
loures90 <fernando.loures@hotmail.com>
2022-04-06 09:13:53 -0300

    feat: ensure LoadSurveysController returns a list of Surveys on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,9 +1,10 @@
+import { ok } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const result = await this.loadSurveys.load()
+    return ok(result)
   }
 }
