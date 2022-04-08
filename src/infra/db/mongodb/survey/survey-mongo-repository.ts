6020e37d8d10af49950e0a3ef2be96a5a86b07b1
loures90 <fernando.loures@hotmail.com>
2022-04-08 13:55:49 -0300

    feat: ensure SurveyMongoRepository returns a list of surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,30 @@
 import { AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
+import { SurveyModel } from '../../../../domain/models/survey'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (addSurveyData: AddSurveyModel): Promise<void> {
     const surveyData = { ...addSurveyData }
     const surveysCollection = await MongoHelper.getCollection('surveys')
     await surveysCollection.insertOne(surveyData)
+  }
+
+  async loadSurveys (): Promise<SurveyModel[]> {
+    const surveysCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveysCollection.find().toArray()
+    const result = []
+    surveys.forEach(survey => {
+      if (survey._id) {
+        result.push({
+          id: survey._id.toString(),
+          question: survey.question,
+          answers: survey.answers,
+          data: survey.data
+        })
+      }
+    })
+    return result
   }
 }
