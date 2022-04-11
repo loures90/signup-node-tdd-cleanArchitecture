eeea95adb770bc66274c6472c6835bc017078483
loures90 <fernando.loures@hotmail.com>
2022-04-11 15:55:16 -0300

    feat: ensure SaveSurveyResultController calls LoadSurveyById correctly

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -0,0 +1,6 @@
+export * from '../../../protocols'
+export * from '../../../protocols/validation'
+export * from '../../../../domain/usecases/survey-result/save-survey-result'
+export * from '../../../../domain/models/survey-result'
+export * from '../../../../domain/models/survey'
+export * from '../../../../domain/usecases/survey/load-survey-by-id'
