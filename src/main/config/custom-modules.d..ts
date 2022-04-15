31d3f9b27dba204721952e74335cc38e40013645
loures90 <fernando.loures@hotmail.com>
2022-04-15 13:50:46 -0300

    chore: add req.account in express module

---
+++ src/main/config/custom-modules.d..ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// eslint-disable-next-line @typescript-eslint/no-namespace
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare module Express {
+  interface Request {
+    accountId?: string
+  }
+}
